821fb65d7f06aa5754f5d9cf9626b8e56c656b19
bundai223 <bundai223@gmail.com>
2018-03-23 10:12:03 +0900

    fix cookbooks

--- cookbooks/neovim/default.rb
+++ cookbooks/neovim/default.rb
@@ -1,8 +1,16 @@
 
 case node[:platform]
 when 'debian', 'ubuntu', 'mint'
+  package 'software-properties-common'
+  execute 'add-apt-repository ppa:neovim-ppa/stable' do
+    run_command 'add-apt-repository ppa:neovim-ppa/stable'
+    run_command 'apt-get update'
+  end
+  package 'python-dev'
+  package 'python-pip'
+  package 'python3-dev'
+  package 'python3-pip'
   package 'neovim'
-  package 'python3-neovim'
 
 when 'fedora', 'redhat', 'amazon'
 when 'osx', 'darwin'
